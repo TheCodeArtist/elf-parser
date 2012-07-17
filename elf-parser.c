7169f9bdc0336274ae0fcaf7123230e72e8a6b30
Chinmay V S <chinmay.v.s@pathpartnertech.com>
2012-07-17 18:52:11 +0530

    Added support for parsing symbol table(s)
    Able to locate and parse the sections containing the symbols in the
    ELF-file. Spec limits the valid sections containing symbols to the
    following 2 sections.
      - SYMTAB : Standard symbol table section.
      - DYNSYM : Dynamic linker symbol table section.
    
    Signed-off-by: Chinmay V S <chinmay.v.s@pathpartnertech.com>

--- elf-parser.c
+++ elf-parser.c
@@ -295,7 +295,7 @@ void print_section_headers(int32_t fd, Elf32_Ehdr eh, Elf32_Shdr sh_table[])
 	char* sh_str;	/* section-header string-table is also a section. */
 
 	/* Read section-header string-table */
-	debug("eh.e_shstrndx=0x%x\n", eh.e_shstrndx);
+	debug("eh.e_shstrndx = 0x%x\n", eh.e_shstrndx);
 	sh_str = read_section(fd, sh_table[eh.e_shstrndx]);
 
 	printf("========================================");
@@ -321,6 +321,50 @@ void print_section_headers(int32_t fd, Elf32_Ehdr eh, Elf32_Shdr sh_table[])
 	printf("\n");	/* end of section header table */
 }
 
+void print_symbol_table(int32_t fd,
+			Elf32_Ehdr eh,
+			Elf32_Shdr sh_table[],
+			uint32_t symbol_table)
+{
+
+	char *str_tbl;
+	Elf32_Sym* sym_tbl;
+	uint32_t i, symbol_count;
+
+	sym_tbl = (Elf32_Sym*)read_section(fd, sh_table[symbol_table]);
+
+	/* Read linked string-table
+	 * Section containing the string table having names of
+	 * symbols of this section
+	 */
+	uint32_t str_tbl_ndx = sh_table[symbol_table].sh_link;
+	debug("str_table_ndx = 0x%x\n", str_tbl_ndx);
+	str_tbl = read_section(fd, sh_table[str_tbl_ndx]);
+
+	symbol_count = (sh_table[symbol_table].sh_size/sizeof(Elf32_Sym));
+	printf("%d symbols\n", symbol_count);
+
+	for(i=0; i< symbol_count; i++) {
+		printf("0x%08x ", sym_tbl[i].st_value);
+		printf("0x%02x ", ELF32_ST_BIND(sym_tbl[i].st_info));
+		printf("0x%02x ", ELF32_ST_TYPE(sym_tbl[i].st_info));
+		printf("%s\n", (str_tbl + sym_tbl[i].st_name));
+	}
+}
+
+void print_symbols(int32_t fd, Elf32_Ehdr eh, Elf32_Shdr sh_table[])
+{
+	uint32_t i;
+
+	for(i=0; i<eh.e_shnum; i++) {
+		if ((sh_table[i].sh_type==SHT_SYMTAB)
+		 || (sh_table[i].sh_type==SHT_DYNSYM)) {
+			printf("\n[Section %03d]", i);
+			print_symbol_table(fd, eh, sh_table, i);
+		}
+	}
+}
+
 /* Main entry point of elf-parser */
 int32_t main(int32_t argc, char *argv[])
 {
@@ -340,25 +384,28 @@ int32_t main(int32_t argc, char *argv[])
 		return 0;
 	}
 
-	/* ELF header at start of file */
+	/* ELF header : at start of file */
 	read_elf_header(fd, &eh);
 	if(!is_ELF(eh)) {
 		return 0;
 	}
 	print_elf_header(eh);
 
-	/* section header table */
+	/* Section header table :  */
 	sh_tbl = malloc(eh.e_shentsize * eh.e_shnum);
 	if(!sh_tbl) {
 		printf("Failed to allocate %d bytes\n",
 			(eh.e_shentsize * eh.e_shnum));
 	}
-
 	read_section_header_table(fd, eh, sh_tbl);
 	print_section_headers(fd, eh, sh_tbl);
 
-
-
+	/* Symbol tables :
+	 * sh_tbl[i].sh_type
+	 * |`- SHT_SYMTAB
+	 *  `- SHT_DYNSYM
+	 */
+	print_symbols(fd, eh, sh_tbl);
 
 	return 0;
 }
